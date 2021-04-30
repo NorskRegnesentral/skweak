a9ca729bc9035730ad968096f4295bb2a5c43032
Pierre Lison <plison@nr.no>
2021-04-30 19:31:52 +0200

    increment version

--- skweak/__init__.py
+++ skweak/__init__.py
@@ -1,5 +1,2 @@
 from . import base, doclevel, gazetteers, heuristics, aggregation, utils, spacy
-
-from pkg_resources import get_distribution
-
-__version__ = get_distribution('skweak').version
+__version__ = "0.2.10"
