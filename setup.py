72ea0e6b2497e59e8e9a2560ff2ff40e4730593b
Pierre Lison <plison@nr.no>
2023-09-20 00:56:15 +0200

    ensuring compatibility with hmmlearn 0.3.0

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='skweak',  
-     version='0.3.2', 
+     version='0.3.3', 
      author="Pierre Lison",
      author_email="plison@nr.no",
      description="Software toolkit for weak supervision in NLP",
      license='LICENSE.txt',
      packages=['skweak'],
      python_requires=">=3.6",
-     install_requires=["spacy>=3.0","hmmlearn>=0.2", "pandas>=0.23", "numpy>=1.18"],
+     install_requires=["spacy>=3.0","hmmlearn>=0.3.0", "pandas>=0.23", "numpy>=1.18"],
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/NorskRegnesentral/skweak",
